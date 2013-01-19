cc3507b3951dc039371f0cc4852f5943f34a5044
jkresner <jkresner@yahoo.com.au>
2013-01-19 10:29:42 -0800

    Basic setup
    - packages.json including express
    - ignore node_modules folder
    - home route welcoming to skilljam

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+  var body = 'Welcome to Skilljam';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+
+
+app.listen(3000);
+console.log('Listening on port 3000');
